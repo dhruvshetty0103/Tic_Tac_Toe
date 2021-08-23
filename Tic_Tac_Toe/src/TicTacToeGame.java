f929c80dac2aa467352f30fe4bfb2d915970290d
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-23 09:00:47 +0530

    Refactored:Use case 1

--- Tic_Tac_Toe/src/TicTacToeGame.java
+++ Tic_Tac_Toe/src/TicTacToeGame.java
@@ -1,19 +1,11 @@
-import java.util.*;
+//import java.util.*;
 public class TicTacToeGame 
 {
-	public static char [] ch={'\0',' ',' ',' ',' ',' ',' ',' ',' ',' '};
-	static void input()
-	{
-		Scanner reader=new Scanner(System.in);
-		for(int i=1;i<10;i++)
-			ch[i] = reader.next().charAt(i); 
-		reader.close();
-	}
+	public static char [] ch={'\0',' ',' ',' ',' ',' ',' ',' ',' ',' '};	
 	public static void main(String[] args) 
 	{
-		System.out.println("Enter elements");
-		input();
-		for(int i=1;i<10;i++)
+		System.out.println("Welcome to TicTacToe");
+		for(int i=1;i<ch.length;i++)
 			System.out.println(ch[i]);
 
 	}
