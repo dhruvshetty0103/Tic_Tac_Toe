4665f9c42b441496b8c319b85f730f533c34b875
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-23 09:21:59 +0530

    Refactored:Use case 1 with initialization according  to length of array

--- Tic_Tac_Toe/src/TicTacToeGame.java
+++ Tic_Tac_Toe/src/TicTacToeGame.java
@@ -1,13 +1,16 @@
-//import java.util.*;
+
 public class TicTacToeGame 
 {
-	public static char [] ch={'\0',' ',' ',' ',' ',' ',' ',' ',' ',' '};	
-	public static void main(String[] args) 
+	public static char [] board=new char[10];
+	static void initialize()
 	{
 		System.out.println("Welcome to TicTacToe");
-		for(int i=1;i<ch.length;i++)
-			System.out.println(ch[i]);
-
+		for(int i=1;i<board.length;i++)
+			board[i]=' ';
+	}
+	public static void main(String[] args) 
+	{
+		
 	}
 
 }
